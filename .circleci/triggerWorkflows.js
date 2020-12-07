fea7d9eeab7ea877e55a80baa45900dcadb7eb81
Dan Levy <danlevy124@gmail.com>
2020-12-07 10:36:46 -0500

    Go Back to Request

--- .circleci/triggerWorkflows.js
+++ .circleci/triggerWorkflows.js
@@ -36,28 +36,20 @@ const triggerWorkflows = async () => {
         parameters[`run-${changedPackage}`] = true;
     });
 
-    // const options = {
-    //     method: "POST",
-    //     url:
-    //         "https://circleci.com/api/v2/project/github/danlevy1/example-design-system/pipeline",
-    //     headers: {
-    //         "content-type": "application/json",
-    //         authorization: "Basic Circle-Token: $CIRCLECI_PERSONAL_TOKEN",
-    //     },
-    //     body: JSON.stringify(parameters),
-    //     json: true,
-    // };
+    const options = {
+        method: "POST",
+        url:
+            "https://circleci.com/api/v2/project/github/danlevy1/example-design-system/pipeline",
+        headers: {
+            "content-type": "application/json",
+            authorization: `Basic Circle-Token: ${process.env.CIRCLECI_TOKEN}`,
+        },
+        body: JSON.stringify(parameters),
+        json: true,
+    };
 
-    // const response = await requestPromise(options);
-    // console.log(response.body, response.statusCode);
-
-    const stdout = await executeShellCommand(
-        `curl --request GET \
-        --url 'https://circleci.com/api/v2/project/gh/danlevy1/example-design-system/pipeline?circle-token=${process.env.CIRCLECI_TOKEN}' \
-        --header 'accept: application/json'`
-    );
-
-    console.log(stdout);
+    const response = await requestPromise(options);
+    console.log(response.body, response.statusCode);
 };
 
 triggerWorkflows();
