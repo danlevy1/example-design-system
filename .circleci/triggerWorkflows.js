d37ccf7b309379aba68482e18a7e686fc4d00b28
Dan Levy <danlevy124@gmail.com>
2020-12-07 10:34:44 -0500

    Try Again

--- .circleci/triggerWorkflows.js
+++ .circleci/triggerWorkflows.js
@@ -36,20 +36,20 @@ const triggerWorkflows = async () => {
         parameters[`run-${changedPackage}`] = true;
     });
 
-    const options = {
-        method: "POST",
-        url:
-            "https://circleci.com/api/v2/project/github/danlevy1/example-design-system/pipeline",
-        headers: {
-            "content-type": "application/json",
-            authorization: "Basic Circle-Token: $CIRCLECI_PERSONAL_TOKEN",
-        },
-        body: JSON.stringify(parameters),
-        json: true,
-    };
+    // const options = {
+    //     method: "POST",
+    //     url:
+    //         "https://circleci.com/api/v2/project/github/danlevy1/example-design-system/pipeline",
+    //     headers: {
+    //         "content-type": "application/json",
+    //         authorization: "Basic Circle-Token: $CIRCLECI_PERSONAL_TOKEN",
+    //     },
+    //     body: JSON.stringify(parameters),
+    //     json: true,
+    // };
 
-    const response = await requestPromise(options);
-    console.log(response.body, response.statusCode);
+    // const response = await requestPromise(options);
+    // console.log(response.body, response.statusCode);
 
     const stdout = await executeShellCommand(
         "curl --request GET \
