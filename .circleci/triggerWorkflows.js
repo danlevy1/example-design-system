2a1050111d83572761204c0e16ef65329fd9537c
Dan Levy <danlevy124@gmail.com>
2020-12-07 10:38:38 -0500

    Lowercase

--- .circleci/triggerWorkflows.js
+++ .circleci/triggerWorkflows.js
@@ -42,7 +42,7 @@ const triggerWorkflows = async () => {
             "https://circleci.com/api/v2/project/github/danlevy1/example-design-system/pipeline",
         headers: {
             "content-type": "application/json",
-            authorization: `Circle-Token: ${process.env.CIRCLECI_TOKEN}`,
+            authorization: `basic Circle-Token: ${process.env.CIRCLECI_TOKEN}`,
         },
         body: JSON.stringify(parameters),
         json: true,
