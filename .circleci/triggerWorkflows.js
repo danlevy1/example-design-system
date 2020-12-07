49956082e319fcdc0604e217971e08c9c11e5acb
Dan Levy <danlevy124@gmail.com>
2020-12-06 19:00:38 -0500

    Update Again

--- .circleci/triggerWorkflows.js
+++ .circleci/triggerWorkflows.js
@@ -42,7 +42,7 @@ const triggerWorkflows = async () => {
             "https://circleci.com/api/v2/project/github/danlevy1/example-design-system",
         headers: {
             "content-type": "application/json",
-            authorization: "Basic ${CIRCLE_CI_API_TOKEN}",
+            authorization: "Circle-Token: ${CIRCLECI_TOKEN}",
         },
         body: JSON.stringify(parameters),
         json: true,
