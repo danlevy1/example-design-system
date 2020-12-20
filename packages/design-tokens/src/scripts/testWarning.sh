18bffacec5aa39b17818ac9a60ecd3e1f9cbcb65
Dan Levy <danlevy124@gmail.com>
2020-12-19 23:04:35 -0500

    Fix Color Output

--- packages/design-tokens/src/scripts/testWarning.sh
+++ packages/design-tokens/src/scripts/testWarning.sh
@@ -4,4 +4,4 @@ set -e
 YELLOW='\033[0;33m'
 END='\e[0m'
 
-printf "${YELLOW}|---------------------------------------------------------------------------------------------------------------------------------|\n| !! Some test files mock console.log, so you might need to use the original console.log for the log to appear in the terminal !! |\n|---------------------------------------------------------------------------------------------------------------------------------|\n\n${END}"
+printf "${YELLOW}|---------------------------------------------------------------------------------------------------------------------------------|\n${YELLOW}| !! Some test files mock console.log, so you might need to use the original console.log for the log to appear in the terminal !! |\n${YELLOW}|---------------------------------------------------------------------------------------------------------------------------------|\n\n${END}"
