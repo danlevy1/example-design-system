45c29a94b6a4a3137fc63f57db2812e3a0d5c976
Dan Levy <danlevy124@gmail.com>
2020-12-22 14:38:00 -0500

    Add Commit and Push

--- scripts/postpublish.sh
+++ scripts/postpublish.sh
@@ -4,9 +4,7 @@ set -e
 packageArr=($(jq -r .name,.version ./package.json))
 packageNameAndVersion="${packageArr[0]}@${packageArr[1]}"
 
-echo "Turn on git commits for ${packageNameAndVersion} when ready"
-
-# git add package.json package-lock.json
-# git commit -m "Publish $packageNameAndVersion"
-# git tag -a $packageNameAndVersion -m "$packageNameAndVersion"
-# git push --follow-tags
+git add package.json package-lock.json
+git commit -m "Publish $packageNameAndVersion"
+git tag -a $packageNameAndVersion -m "$packageNameAndVersion"
+git push --follow-tags
