d7f431e3a4b9f382af174849fbb56b12f3dd34ae
Dan Levy <danlevy124@gmail.com>
2020-12-22 21:26:11 -0500

    Again

--- .circleci/publishPackages.sh
+++ .circleci/publishPackages.sh
@@ -12,52 +12,44 @@ END='\e[0m'
 
 # Returns an array of JSON objects, where each object is of the following shape:
 # { name: package-name-without-scope, localVersion: version-from-package-json, publishedVersion: version-on-npm@latest }
-getPackageVersions () {
-    local packageNames=($( ls ./packages ))
-    local packageVersions=()
 
-    for packageName in "${packageNames[@]}"
-    do
-        local name=$packageName
-        local localVersion=$( jq -r .version ./packages/$name/package.json )
-        local publishedVersion=$( npm view @x3r5e/$name version)
+packageNames=($( ls ./packages ))
+packageVersions=()
 
-        packageVersions+=($( jq -nc \
-                                 --arg name "${name}" \
-                                 --arg localVersion "${localVersion}" \
-                                 --arg publishedVersion "${publishedVersion}" \
-                                 '{name: $name, localVersion: $localVersion, publishedVersion: $publishedVersion}' ) )
-    done
+for packageName in "${packageNames[@]}"
+do
+    local name=$packageName
+    local localVersion=$( jq -r .version ./packages/$name/package.json )
+    local publishedVersion=$( npm view @x3r5e/$name version)
 
-    echo ${packageVersions[@]}
-}
+    packageVersions+=($( jq -nc \
+                                --arg name "${name}" \
+                                --arg localVersion "${localVersion}" \
+                                --arg publishedVersion "${publishedVersion}" \
+                                '{name: $name, localVersion: $localVersion, publishedVersion: $publishedVersion}' ) )
+done
 
 # Accepts an array of package version JSON objects, where each object is of the following shape:
 # { name: package-name-without-scope, localVersion: version-from-package-json, publishedVersion: version-on-npm@latest }
 # Returns an array of package names that need to be published
-getPackageNamesToPublish () {
-    local packageVersions=("$@")
-    local packagesToPublish=()
+packagesToPublish=()
 
-    for packageVersion in "${packageVersions[@]}"
-    do        
-        local localVersion=$( jq -r .localVersion <<< $packageVersion)
-        local publishedVersion=$( jq -r .publishedVersion <<< $packageVersion )
+for packageVersion in "${packageVersions[@]}"
+do        
+    local localVersion=$( jq -r .localVersion <<< $packageVersion)
+    local publishedVersion=$( jq -r .publishedVersion <<< $packageVersion )
 
-        if [ $localVersion != $publishedVersion ]
-        then
-            local name=$( jq -r .name <<< $packageVersion )
-            packagesToPublish+=($name)
-        fi
-    done
+    if [ $localVersion != $publishedVersion ]
+    then
+        local name=$( jq -r .name <<< $packageVersion )
+        packagesToPublish+=($name)
+    fi
+done
 
-    echo ${packagesToPublish[@]}
-}
+echo ${packageVersions[@]}
+echo ${packagesToPublish[@]}
 
-packageVersions=($( getPackageVersions ))
-packageNamesToPublish=($( getPackageNamesToPublish "${packageVersions[@]}" ))
-
-echo ${packageNamesToPublish[@]}
+# Publish the packages
 
 # If none of the packages have had their version changed, skip publishing
 if [ -z "${packageNamesToPublish[@]}" ]
