031ce77b140774ee66d6406e549c1528505d1920
Dan Levy <danlevy124@gmail.com>
2020-12-22 20:38:40 -0500

    Add git add Check

--- scripts/postpublish.sh
+++ scripts/postpublish.sh
@@ -4,7 +4,13 @@ set -e
 packageArr=($(jq -r .name,.version ./package.json))
 packageNameAndVersion="${packageArr[0]}@${packageArr[1]}"
 
-git add package.json package-lock.json
-git commit -m "Publish $packageNameAndVersion"
-git tag -a $packageNameAndVersion -m "$packageNameAndVersion"
+changedFiles=$( git add package.json package-lock.json --dry-run )
+
+if [[ "$changedFiles" ]]
+then
+    git add package.json package-lock.json
+    git commit -m "Publish $packageNameAndVersion"
+fi
+
+git tag -a "$packageNameAndVersion" -m "$packageNameAndVersion"
 git push --follow-tags
