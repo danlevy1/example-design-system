f4c7d5500df4201c88e891a8df08b94f484fd8f7
Dan Levy <danlevy124@gmail.com>
2021-02-04 22:35:38 -0500

    More Changes

--- packages/react-components/rollup.config.js
+++ packages/react-components/rollup.config.js
@@ -1,44 +1,50 @@
 import commonjs from "@rollup/plugin-commonjs";
 import del from "rollup-plugin-delete";
 import cleanup from "rollup-plugin-cleanup";
-import externals from "rollup-plugin-node-externals";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import babel from "@rollup/plugin-babel";
 import typescript from "rollup-plugin-typescript2";
 import json from "@rollup/plugin-json";
 import pkg from "./package.json";
 
+const external = [
+    ...Object.keys(pkg.dependencies),
+    ...Object.keys(pkg.peerDependencies),
+];
+
+const extensions = [".js", ".jsx", ".ts", ".tsx"];
+
 const plugins = [
     del({
         targets: "dist/*",
     }),
     json(),
-    externals({
-        packagePath: "./package.json",
-        include: ["@babel/runtime-corejs3"],
-    }),
-    nodeResolve(),
+    nodeResolve({ extensions }),
     commonjs(),
-    typescript({ useTsconfigDeclarationDir: true }),
-    babel({ exclude: "node_modules/**", babelHelpers: "runtime" }),
+    babel({ exclude: "node_modules/**", babelHelpers: "runtime", extensions }),
     cleanup({ comments: "jsdoc", maxEmptyLines: 1, sourcemap: false }),
 ];
 
-export default {
-    input: "src/index.ts",
-    output: [
-        {
-            file: pkg.main,
-            format: "cjs",
-            sourcemap: true,
-            exports: "named",
-        },
-        {
-            file: pkg.module,
-            format: "esm",
-            sourcemap: true,
-            exports: "named",
-        },
-    ],
-    plugins,
-};
+export default [
+    {
+        input: "src/index.ts",
+        output: [
+            {
+                file: pkg.main,
+                format: "cjs",
+                exports: "named",
+            },
+            {
+                file: pkg.module,
+                format: "esm",
+                exports: "named",
+            },
+        ],
+        plugins,
+        external,
+    },
+    {
+        input: "src/index.ts",
+        plugins: [typescript({ useTsconfigDeclarationDir: true })],
+    },
+];
