4c96d87650a065f5b9be65fc30a3d1beafa4861d
Dan Levy <danlevy124@gmail.com>
2021-02-10 22:53:28 -0500

    Add CSS Module Support

--- packages/react-components/.storybook/main.js
+++ packages/react-components/.storybook/main.js
@@ -1,7 +1,25 @@
 module.exports = {
+    webpackFinal: async (config) => {
+        /*
+         * Ignores all node modules except for those in the "@x3r5e" org.
+         * See https://stackoverflow.com/a/59954986/6063755
+         */
+        config.watchOptions = {
+            ignored: [
+                /node_modules\/(?!@x3r5e\/.+)/,
+                /\@x3r5e\/.+\/node_modules/,
+            ],
+        };
+
+        return config;
+    },
     stories: [
-        "../src/**/*.stories.mdx",
-        "../src/**/*.stories.@(js|jsx|ts|tsx)",
+        "../src/components/**/*.stories.mdx",
+        "../src/components/**/*.stories.@(js|jsx|ts|tsx)",
     ],
-    addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+    addons: [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "storybook-css-modules-preset",
+    ],
 };
---
+++ packages/react-components/src/components/Buttton/Button.stories.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Meta } from "@storybook/react/types-6-0";
+import { Button, ButtonProps } from "./Button";
+
+export const Primary: React.FC<ButtonProps> = () => <Button>Button</Button>;
+
+export default {
+    title: "Components/Button",
+    component: Button,
+} as Meta;
