d58c92a03bf761f596cad64c0b5b022b66d26eb5
Dan Levy <danlevy124@gmail.com>
2021-02-24 21:19:29 -0500

    Fix

--- scripts/postpublish.sh
+++ scripts/postpublish.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 set -e
 
-args=("$@")
-packageName=${args[0]}
-
-cat ./package.json
-
-packageArr=($(jq -r .name,.version ./packages/"$packageName"/package.json))
+packageArr=($(jq -r .name,.version ./package.json))
 packageNameAndVersion="${packageArr[0]}@${packageArr[1]}"
 
-changedFiles=$( git add ./packages/"$packageName"/package.json ./packages/"$packageName"/package-lock.json --dry-run )
+changedFiles=$( git add package.json package-lock.json --dry-run )
 
 if [[ "$changedFiles" ]]
 then
-    git add ./packages/"$packageName"/package.json ./packages/"$packageName"/package-lock.json
+    git add package.json package-lock.json
 fi
 
 git tag -a "$packageNameAndVersion" -m "$packageNameAndVersion"
