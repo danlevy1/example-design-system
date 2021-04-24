204353ef5ea09a27b11bfd359f901c94e9f6dcdc
Dan Levy <danlevy124@gmail.com>
2021-04-24 14:48:11 -0400

    Update Styles

--- packages/react-components/src/components/Buttton/Button.module.scss.d.ts
+++ packages/react-components/src/components/Buttton/Button.module.scss.d.ts
@@ -2,6 +2,14 @@
 
 export type Styles = {
     button: string;
+    icon: string;
+    label: string;
+    large: string;
+    medium: string;
+    secondary: string;
+    small: string;
+    spinner: string;
+    tertiary: string;
 };
 
 export type ClassNames = keyof Styles;
--- packages/react-components/src/components/Buttton/Button.stories.tsx
+++ packages/react-components/src/components/Buttton/Button.stories.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import { Meta } from "@storybook/react/types-6-0";
 import { Button, ButtonProps } from "./Button";
 
-export const Primary: React.FC<ButtonProps> = () => <Button>Button</Button>;
+export const Primary: React.FC<ButtonProps> = () => (
+    <Button size="large" label="Button" />
+);
 
 export default {
     title: "Components/Button",
